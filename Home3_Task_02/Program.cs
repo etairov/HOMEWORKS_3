99d13f5a311bb6c58315b82ef055df00c1f3a16a
e.tairov <ev.tairov@yandex.ru>
2023-01-25 19:44:21 +0700

    3D- workspace

--- Home3_Task_02/Program.cs
+++ Home3_Task_02/Program.cs
@@ -3,3 +3,20 @@
 //A (3,6,8); B (2,1,-7), -> 15.84 A (7,-5, 0); B (1,-1,9) -> 11.53
 //------------------------------------------------------------------
 
+    int ax = new Random().Next(-100, 100);
+    int ay = new Random().Next(-100, 100);
+    int az = new Random().Next(-100, 100);
+
+    int bx = new Random().Next(-100, 100);
+    int by = new Random().Next(-100, 100);
+    int bz = new Random().Next(-100, 100);
+
+double ourDistance(int ax, int ay, int az, int bx, int by, int bz)
+{
+    double result = Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2) + Math.Pow(az - by, 2));
+    return result;
+}
+
+double distance = ourDistance(ax, ay, az, bx, by, bz);
+
+Console.WriteLine($"Расстояние между точками ({ax},{ay},{az}) и ({bx},{by},{bz}) равно {distance:f3}");
