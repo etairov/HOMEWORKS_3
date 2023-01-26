77c7764660e3e9c5848135329cb35e1131458271
e.tairov <ev.tairov@yandex.ru>
2023-01-26 15:33:53 +0700

    GetCubeNumber

--- Home3_Task_03/Program.cs
+++ Home3_Task_03/Program.cs
@@ -3,3 +3,25 @@
 //3 -> 1, 8, 27 5 -> 1, 8, 27, 64, 125
 //------------------------------------
 
+int GetNumber()
+{
+    Console.WriteLine("Введите любое число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int resultGetNumber = GetNumber();
+
+    int N = resultGetNumber;
+    Console.Write($"{resultGetNumber} --> ");
+
+    for (int i = 1; i <= N; i++)
+    {
+        if (i < N) 
+        {
+             Console.Write($"{Math.Pow(i, 3)}, ");    
+        }
+        else
+        {
+             Console.Write($"{Math.Pow(i, 3)}");
+        }
+    }
